c04f3e3bc426d68b2c12789da635f87577521381
Kevin Magliocchi <104865761+Kevin-Magli@users.noreply.github.com>
2026-03-07 13:24:27 +0000

    Nivel Aventureiro -  Concluido!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,8 @@ int main() {
     float area1;
     float pib1;
     int pontos_turisticos1;
+    float densidade_populacional1;
+    float pib_per_capita1;
 
     // Variáveis para a Carta 2
     char estado2;
@@ -18,6 +20,8 @@ int main() {
     float area2;
     float pib2;
     int pontos_turisticos2;
+    float densidade_populacional2;
+    float pib_per_capita2;
 
     // Cadastro da Carta 1
     printf("--- Cadastro da Carta 1 ---\n");
@@ -55,6 +59,14 @@ int main() {
     scanf("%d", &pontos_turisticos2);
     printf("\n");
 
+    // Cálculos para a Carta 1
+    densidade_populacional1 = (float)populacao1 / area1;
+    pib_per_capita1 = (pib1 * 1000000000) / populacao1;
+
+    // Cálculos para a Carta 2
+    densidade_populacional2 = (float)populacao2 / area2;
+    pib_per_capita2 = (pib2 * 1000000000) / populacao2;
+
     // Exibição dos dados das cartas
     printf("--- Dados Cadastrados ---\n");
     printf("Carta 1:\n");
@@ -65,6 +77,8 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional1);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita1);
     printf("\n");
 
     printf("Carta 2:\n");
@@ -75,6 +89,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional2);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita2);
 
     return 0;
 }
